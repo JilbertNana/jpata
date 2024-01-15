659ceebbd006c154b4113e85b81c350a064fc812
JilbertNana <156682861+JilbertNana@users.noreply.github.com>
2024-01-15 16:48:12 +0100

    Add files via upload

---
+++ ex00.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    int somma = 0;
+
+
+    for(int a = 2; a <= 100; a += 2)
+    {
+        somma += a;
+    
+    }
+
+    
+   
+printf("%d\n", somma);
+
+}
+
