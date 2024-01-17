5dfbc313205e984a79fa3089bd3fb938b031ad47
JilbertNana <156682861+JilbertNana@users.noreply.github.com>
2024-01-17 09:53:47 +0100

    Add files via upload

--- ex08.c
+++ ex08.c
@@ -1,1 +1,27 @@
-inclu
+#include <stdio.h>
+
+int main(){
+
+    int a;
+
+    printf("quando sei nato? \n");
+    scanf("%d" , &a);
+
+    if(a>1969){
+
+    printf("sei nato %d" , a-1969);
+    printf(" anni dopo il primo passo sulla luna \n");
+
+    }
+    else if(a<1969){
+
+        printf("sei nato %d" , 1969-a);
+
+    }
+    else{
+        printf("sei nato lo stesso anno del primo passo sulla luna \n");
+    }
+
+    return 0;
+
+}
