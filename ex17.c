37cd2cd8bf6396aeeae28850866067087d152bc7
PIPPOCHAMPAGNEVIVALAFRESCA <156682861+JilbertNana@users.noreply.github.com>
2024-01-18 17:06:41 +0100

    Add files via upload

---
+++ ex17.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+ int main(){
+    char parola[7]={'f', 'r', 'e', 's', 'c', 'a','!'};
+    printf("ballamose la %s \n", parola);
+return 0;
+
+}
---
+++ ex18.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    char stringa[]= "calciatore più forte del mondo";
+    char messi[6]= {'m', 'e', 's', 's', 'i', '\0'};
+    char gol [7]= {'g', 'o', 'l', '8', '4', '8', '\0'};
+    char nazionalità[]= "nazionalità argentina";
+
+    printf("%s \n", stringa );
+    printf("%s \n", messi );
+    printf("%s \n", gol );
+    printf("%s \n", nazionalità );
+return 0;
+
+}
